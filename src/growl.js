2aa5298281b87035a34cf14a7a04fb68ca24c3a0
Petr Hoskovec <ihoskovecpetr@gmail.com>
2022-03-06 15:39:59 +1100

    Hooks updated for self-closing logic

--- src/growl.js
+++ src/growl.js
@@ -1,25 +1,36 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
 
-import './growl.css'
+import "./growl.css";
 
 export const Growl = ({ active, message, onDismissed }) => (
   <div className={`growl${active ? " active" : ""}`}>
     {message}
     <div onClick={onDismissed} className="growl-close" />
   </div>
-)
+);
 
-export function useGrowl() {
-    // state of the growl
-    const [growlActive, setGrowlActive] = React.useState(false)
+export function useGrowl({ isSelfClosing = false, selfClosingPeriod = 3000 }) {
+  // state of the growl
+  const [growlActive, setGrowlActive] = React.useState(false);
+  const timeoutRef = useRef(null);
 
-    return [
-        // the first arg is the state
-        growlActive, 
+  useEffect(() => {
+    if (isSelfClosing && growlActive) {
+      timeoutRef.current = setTimeout(() => {
+        setGrowlActive(false);
+      }, [selfClosingPeriod]);
+    } else {
+      clearTimeout(timeoutRef.current);
+    }
+  }, [growlActive]);
 
-        // the second arg is a fn that allows you to safely set its state
-        (active) => {
-            setGrowlActive(active)
-        },
-    ]
+  return [
+    // the first arg is the state
+    growlActive,
+
+    // the second arg is a fn that allows you to safely set its state
+    (active) => {
+      setGrowlActive(active);
+    },
+  ];
 }
